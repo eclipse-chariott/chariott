f544aab96b21c7e132fcc3b39e435598e69b6a31
github-action-benchmark <github@users.noreply.github.com>
2022-10-27 14:21:20 +0000

    add Chariott Benchmark (customSmallerIsBetter) benchmark result for 02ae33d40eb06dbff4a56f7ee150cca84bd62cec

---
+++ dev/bench/data.js
@@ -0,0 +1,86 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1666880480924,
+  "repoUrl": "https://github.com/eclipse/chariott",
+  "entries": {
+    "Chariott Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "code@raboof.com",
+            "name": "Atif Aziz",
+            "username": "atifaziz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "02ae33d40eb06dbff4a56f7ee150cca84bd62cec",
+          "message": "Trim trailing whitespace from license/notice (#6)",
+          "timestamp": "2022-10-27T16:04:50+02:00",
+          "tree_id": "6debd077818e3dc7447937b96cf58a9205e35e31",
+          "url": "https://github.com/eclipse/chariott/commit/02ae33d40eb06dbff4a56f7ee150cca84bd62cec"
+        },
+        "date": 1666880480221,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Definitely Lost Bytes",
+            "value": "0",
+            "unit": "bytes"
+          },
+          {
+            "name": "Definitely Lost Blocks",
+            "value": "0",
+            "unit": "blocks"
+          },
+          {
+            "name": "Indirectly Lost Bytes",
+            "value": "0",
+            "unit": "bytes"
+          },
+          {
+            "name": "Indirectly Lost Blocks",
+            "value": "0",
+            "unit": "blocks"
+          },
+          {
+            "name": "Possibly Lost Bytes",
+            "value": "1960",
+            "unit": "bytes"
+          },
+          {
+            "name": "Possibly Lost Blocks",
+            "value": "7",
+            "unit": "blocks"
+          },
+          {
+            "name": "CPU Usage",
+            "value": 31,
+            "unit": "Percent",
+            "range": "p95=32"
+          },
+          {
+            "name": "Memory Usage",
+            "value": 6492,
+            "unit": "Bytes",
+            "range": "p95=6680"
+          },
+          {
+            "name": "Invoke Throughput",
+            "value": 3030,
+            "unit": "1/Second",
+            "range": null
+          },
+          {
+            "name": "Latency",
+            "value": 6,
+            "unit": "Millisecond",
+            "range": "p95=10"
+          }
+        ]
+      }
+    ]
+  }
+}
