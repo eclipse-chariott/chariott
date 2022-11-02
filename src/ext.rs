d9ad51448e195ae98a927cf8a0fb7f1bd9814c3e
Atif Aziz <code@raboof.com>
2022-11-02 12:25:05 +0000

    Add tests for "ok" extension

--- src/ext.rs
+++ src/ext.rs
@@ -7,3 +7,35 @@ impl<T, E> OptionExt<T, E> for Option<Result<T, E>> {
         self.map_or(Ok(None), |r| r.map(Some))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, PartialEq, Eq)]
+    struct Error;
+
+    #[test]
+    pub fn ok_with_none_returns_ok_of_some() {
+        let input: Option<Result<(), Error>> = None;
+        let result = input.ok();
+        assert!(result.is_ok());
+        assert_eq!(None, result.unwrap());
+    }
+
+    #[test]
+    pub fn ok_with_some_ok_returns_ok_of_some() {
+        let input: Option<Result<_, Error>> = Some(Ok(42));
+        let result = input.ok();
+        assert!(result.is_ok());
+        assert_eq!(Some(42), result.unwrap());
+    }
+
+    #[test]
+    pub fn ok_with_some_err_returns_err_of_some() {
+        let input: Option<Result<(), Error>> = Some(Err(Error));
+        let result = input.ok();
+        assert!(result.is_err());
+        assert_eq!(Error, result.unwrap_err());
+    }
+}
