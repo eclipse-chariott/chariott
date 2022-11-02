159bb86c9f8ba5233f26fafad288bdfb2f9aba55
Bastian Burger <baburger@microsoft.com>
2022-11-02 10:45:58 +0000

    Add file header

--- common/src/ess.rs
+++ common/src/ess.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 use std::{sync::Arc, time::SystemTime};
 
 use crate::proto::{
--- src/ess.rs
+++ src/ess.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 use chariott_common::ess::Ess as InnerEss;
 
 pub type Ess = InnerEss<()>;
