9ad225f6e91b2889cf93f5f41d85624c7b59338a
Bastian Burger <baburger@microsoft.com>
2022-11-17 17:04:56 +0100

    Simplify Car Bridge modules

--- car-bridge/src/lib.rs
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-pub mod chariott;
-pub mod messaging;
--- car-bridge/src/main.rs
+++ car-bridge/src/main.rs
@@ -1,19 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-use std::error::Error;
-
-use car_bridge::{
-    chariott::fulfill,
-    messaging::{MqttMessaging, Publisher, Subscriber},
-};
 use chariott_common::{
     chariott_api::{ChariottCommunication, GrpcChariott},
     config::env,
+    error::Error,
     shutdown::ctrl_c_cancellation,
 };
-use paho_mqtt::{Message, MessageBuilder, Properties, PropertyCode, QOS_2};
-use prost::Message as _;
+use chariott_proto::{common::IntentEnum, runtime::FulfillRequest};
+use messaging::{MqttMessaging, Publisher, Subscriber};
+use paho_mqtt::{Message as MqttMessage, MessageBuilder, Properties, PropertyCode, QOS_2};
+use prost::Message;
 use tokio::{
     select, spawn,
     sync::mpsc::{self, Sender},
@@ -22,11 +19,14 @@ use tokio_stream::StreamExt as _;
 use tracing::{error, Level};
 use tracing_subscriber::{util::SubscriberInitExt as _, EnvFilter};
 
+mod messaging;
+
 const VIN_ENV_NAME: &str = "VIN";
 const DEFAULT_VIN: &str = "1";
+const PUBLISH_BUFFER: usize = 50;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::builder().with_default_directive(Level::INFO.into()).from_env_lossy(),
@@ -44,7 +44,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let cancellation_token = ctrl_c_cancellation();
 
-    let (response_sender, mut response_receiver) = mpsc::channel(50);
+    // Detach sending the responses from handling the messages to avoid
+    // backpressure.
+
+    let (response_sender, mut response_receiver) = mpsc::channel(PUBLISH_BUFFER);
 
     spawn(async move {
         while let Some((topic, message)) = response_receiver.recv().await {
@@ -60,8 +63,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 if let Some(message) = message {
                     let mut chariott = chariott.clone();
                     let response_sender = response_sender.clone();
-
-                    // Handle message as separate task to avoid backpressure.
 
                     spawn(async move {
                         handle_message(&mut chariott, response_sender, message).await;
@@ -83,26 +84,48 @@ async fn main() -> Result<(), Box<dyn Error>> {
 async fn handle_message(
     chariott: &mut impl ChariottCommunication,
     response_sender: Sender<(String, MessageBuilder)>,
-    message: Message,
+    message: MqttMessage,
 ) {
     async fn inner(
         chariott: &mut impl ChariottCommunication,
         response_sender: Sender<(String, MessageBuilder)>,
-        message: Message,
-    ) -> Result<(), Box<dyn Error>> {
-        let response = fulfill(chariott, message.payload()).await?;
+        message: MqttMessage,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let correlation_data = message
+            .properties()
+            .get_binary(PropertyCode::CorrelationData)
+            .ok_or_else(|| Error::new("No correlation data found on message."))?;
+
+        let response_topic = message
+            .properties()
+            .get_string(PropertyCode::ResponseTopic)
+            .ok_or_else(|| Error::new("No correlation data found on message."))?;
+
+        let fulfill_request: FulfillRequest = Message::decode(message.payload())?;
+
+        let intent_enum = fulfill_request
+            .intent
+            .and_then(|intent| intent.intent)
+            .ok_or_else(|| Error::new("Message does not contain an intent."))?;
+
+        let response = match intent_enum {
+            IntentEnum::Discover(_) => Err(Error::new("Discover is not supported.")),
+            IntentEnum::Subscribe(_) => todo!(),
+            _ => chariott
+                .fulfill(fulfill_request.namespace, intent_enum)
+                .await
+                .map(|r| r.into_inner()),
+        }?;
 
         let mut buffer = vec![];
         response.encode(&mut buffer)?;
 
         let mut properties = Properties::new();
-        properties.push_binary(
-            PropertyCode::CorrelationData,
-            message.properties().get_binary(PropertyCode::CorrelationData).unwrap(),
-        )?;
+        properties.push_binary(PropertyCode::CorrelationData, correlation_data)?;
 
-        let topic = message.properties().get_string(PropertyCode::ResponseTopic).unwrap();
-        response_sender.send((topic, MessageBuilder::new().payload(buffer).qos(QOS_2))).await?;
+        response_sender
+            .send((response_topic, MessageBuilder::new().payload(buffer).qos(QOS_2)))
+            .await?;
 
         Ok(())
     }
