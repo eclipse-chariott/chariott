4c9350ffc2d2523e87ccd9eec993716f314a3564
Bastian Burger <baburger@microsoft.com>
2022-11-17 10:26:37 +0100

    Add layer for MQTT communication

--- car-bridge/src/main.rs
+++ car-bridge/src/main.rs
@@ -1,20 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-use std::{env, error::Error, time::Duration};
+use std::{env, error::Error};
 
+use car_bridge::messaging::{Messaging, MqttMessaging};
 use chariott_common::shutdown::ctrl_c_cancellation;
-use paho_mqtt::{AsyncClient, ConnectOptionsBuilder, CreateOptionsBuilder, MQTT_VERSION_5, QOS_2};
-use tokio::{select, time::sleep};
+use tokio::select;
 use tokio_stream::StreamExt as _;
 use tracing::{info, Level};
 use tracing_subscriber::{util::SubscriberInitExt as _, EnvFilter};
 
 const VIN_ENV_NAME: &str = "VIN";
 const DEFAULT_VIN: &str = "1";
-const BROKER_URL_ENV_NAME: &str = "BROKER_URL";
-const DEFAULT_BROKER_URL: &str = "tcp://localhost:1883";
-const MQTT_CLIENT_BUFFER_SIZE: usize = 25;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -26,43 +23,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .init();
 
     let vin = env::var(VIN_ENV_NAME).unwrap_or_else(|_| DEFAULT_VIN.to_owned());
-    let host = env::var(BROKER_URL_ENV_NAME).unwrap_or_else(|_| DEFAULT_BROKER_URL.to_owned());
 
-    info!("Connecting to MQTT broker on '{host}'.");
-
-    let mut client = AsyncClient::new(
-        CreateOptionsBuilder::new()
-            .mqtt_version(MQTT_VERSION_5)
-            .server_uri(host)
-            .client_id(format!("car-bridge-{vin}"))
-            .finalize(),
-    )?;
-
-    let mut message_stream = client.get_stream(MQTT_CLIENT_BUFFER_SIZE);
-
-    client.connect(ConnectOptionsBuilder::new().mqtt_version(MQTT_VERSION_5).finalize()).await?;
-
-    let c2d_topic = format!("c2d/{vin}");
-    info!("Subscribing to topic '{c2d_topic}'.");
-    client.subscribe(c2d_topic, QOS_2).await?;
+    let client = MqttMessaging::connect(format!("c2d/{vin}")).await?;
+    let mut messages = client.receive().await;
 
     let cancellation_token = ctrl_c_cancellation();
 
     loop {
         select! {
-            message = message_stream.next() => {
+            message = messages.next() => {
                 if let Some(message) = message {
-                    if let Some(message) = message {
-                        info!("(R) {message:?}");
-                    }
-                    else {
-                        info!("Connection temporarily lost.");
-
-                        while let Err(err) = client.reconnect().await {
-                            info!("Trying to reconnect: {}.", err);
-                            sleep(Duration::from_secs(5)).await;
-                        }
-                    }
+                    info!("(R) {message:?}");
                 }
                 else {
                     break;
@@ -73,9 +44,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     }
-
-    info!("Disconnecting the client.");
-    client.disconnect(None).await?;
 
     Ok(())
 }
---
+++ car-bridge/src/messaging.rs
@@ -0,0 +1,113 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+use std::{env, time::Duration};
+
+use async_channel::Receiver;
+use async_stream::stream;
+use async_trait::async_trait;
+use chariott_common::error::{Error, ResultExt as _};
+use futures::{stream::BoxStream, StreamExt as _};
+use paho_mqtt::{
+    AsyncClient, ConnectOptionsBuilder, CreateOptionsBuilder, Message, MQTT_VERSION_5, QOS_2,
+};
+use tracing::info;
+
+#[async_trait]
+pub trait Messaging {
+    type Message;
+
+    async fn receive<'a>(&'a self) -> BoxStream<'a, Self::Message>;
+    async fn send(&self, message: Self::Message) -> Result<(), Error>;
+}
+
+pub struct MqttMessaging {
+    client: AsyncClient,
+    receiver: Receiver<Option<Message>>,
+}
+
+impl Drop for MqttMessaging {
+    fn drop(&mut self) {
+        // Best-effort disconnect.
+        _ = self.client.disconnect(None).wait();
+    }
+}
+
+impl MqttMessaging {
+    pub async fn connect(topic: String) -> Result<Self, Error> {
+        const BROKER_URL_ENV_NAME: &str = "BROKER_URL";
+        const DEFAULT_BROKER_URL: &str = "tcp://localhost:1883";
+        const MQTT_CLIENT_BUFFER_SIZE: usize = 25;
+
+        let host = env::var(BROKER_URL_ENV_NAME).unwrap_or_else(|_| DEFAULT_BROKER_URL.to_owned());
+
+        info!("Connecting to MQTT broker on '{host}' and subscribing to '{topic}'.");
+
+        let mut client = AsyncClient::new(
+            CreateOptionsBuilder::new()
+                .mqtt_version(MQTT_VERSION_5)
+                .server_uri(host)
+                .client_id(format!("car-bridge-{topic}"))
+                .finalize(),
+        )
+        .map_err_with("Failed to create MQTT client.")?;
+
+        // Get the stream before connecting the client, as otherwise messages
+        // may be lost. If the Car Bridge restarts while the number of
+        // outstanding messages is larger than the buffer size, messages can be
+        // lost.
+
+        let receiver = client.get_stream(MQTT_CLIENT_BUFFER_SIZE);
+
+        // TODO: if the broker goes down and does not persist the session, the
+        // client must reestablish the subscriptions.
+
+        client
+            .connect(
+                ConnectOptionsBuilder::new()
+                    .mqtt_version(MQTT_VERSION_5)
+                    .automatic_reconnect(Duration::from_secs(1), Duration::from_secs(60))
+                    .finalize(),
+            )
+            .await
+            .map_err_with("Could not connect to MQTT broker.")?;
+
+        // C2D messages must be delivered with QOS 2, as we can not guarantee
+        // that the fulfill requests they contain are always idempotent.
+
+        client
+            .subscribe(topic, QOS_2)
+            .await
+            .map_err_with("Could not subscribe to topic for receiving C2D messages.")?;
+
+        Ok(Self { client, receiver })
+    }
+}
+
+#[async_trait]
+impl Messaging for MqttMessaging {
+    type Message = Message;
+
+    async fn receive<'a>(&'a self) -> BoxStream<'a, Self::Message> {
+        let mut receiver = self.receiver.clone();
+
+        let s = stream! {
+            while let Some(message) = receiver.next().await {
+                if let Some(message) = message {
+                    yield message;
+                }
+                else {
+                    // Automatic reconnect is used when connecting the
+                    // `AsyncClient`.
+                    info!("Connection temporarily lost. Attempting automatic reconnect.");
+                }
+            }
+        };
+
+        s.boxed()
+    }
+
+    async fn send(&self, message: Self::Message) -> Result<(), Error> {
+        self.client.publish(message).await.map_err_with("Error when publishing a response.")
+    }
+}
