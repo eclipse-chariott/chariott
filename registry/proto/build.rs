e66986559e697bab08f07f3318fb895905b5d683
Lauren Datz <105828115+ladatz@users.noreply.github.com>
2023-06-07 13:12:27 -0400

    Initial Service Discovery

---
+++ registry/proto/build.rs
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+// SPDX-License-Identifier: MIT
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::compile_protos("../proto/chariott/v1/chariott_registry.proto")?;
+    Ok(())
+}
---
+++ registry/proto/src/lib.rs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+// SPDX-License-Identifier: MIT
+
+pub mod chariott_registry {
+    pub mod v1 {
+        #![allow(clippy::derive_partial_eq_without_eq)]
+        tonic::include_proto!("chariott_registry");
+    }
+}
